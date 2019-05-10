040555c6eab4a60b67b1bd5b1dd4a05ebc18fbd2
Gustavo <gustavohenriquesilva124@gmail.com>
2019-05-10 11:23:26 -0300

    atualização

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var musicas = [
     { name: "AWAY & Midoca ", src: "Biblioteca/musica3.mp3", imagem: "fundo/imagem3.png" },
     { name: "Lux Holm & Glaceo ", src: "Biblioteca/musica4.mp3", imagem: "fundo/imagem4.jpg" }
 ];
-var r =0;
+var r = 0;
 //FUNÇÃO PARA TROCAR A MUSICA OU VOLTAR
 function troca(number) {
     var audio = document.getElementById("audio")
@@ -15,27 +15,27 @@ function troca(number) {
     document.getElementById("imagem").src = musicas[number].imagem;
     document.getElementById("tocada").innerHTML = musicas[number].name;
     conversao()
-        current()
-    
+    current()
+
 }
 // BOTAO PARA PROXIMA MUSICA
 var i = 0
 
 function next() {
-    
+
     i++
     minunts = 0
     document.getElementById("playpause").innerHTML = playpause[0].src
     f = 0
-   
-        if (i < 0) {
-            i = 3;
-        }
-        if (i > 3) {
-            i = 0
-        }
-        troca(i)
-    
+
+    if (i < 0) {
+        i = 3;
+    }
+    if (i > 3) {
+        i = 0
+    }
+    troca(i)
+
 }
 // BOTAO PARA RETORNAR A MUSICA
 function turn() {
@@ -47,7 +47,7 @@ function turn() {
     }
     if (i > 3) {
         i = 0
-        
+
     }
     troca(i)
 }
@@ -66,7 +66,7 @@ function play() {
     else if (f % 2 != 0) {
         document.getElementById("playpause").innerHTML = playpause[1].src
         document.getElementById("audio").pause()
-        
+
     }
 }
 // BOTAO DE STOP
@@ -75,12 +75,12 @@ function stop() {
     document.getElementById("audio").pause();
     document.getElementById("playpause").innerHTML = playpause[1].src
     f = 1
-    document.getElementById("progress_bar").style.width = 0+"%"
+    document.getElementById("progress_bar").style.width = 0 + "%"
 }
 
 // COMANDO DE LOOP
 L = 1;
-var r=0;    
+var r = 0;
 function loop() {
     L++
     if (L % 2 == 0) {
@@ -88,12 +88,12 @@ function loop() {
         document.getElementById('reload').style.backgroundColor = "#373737";
         document.getElementById('reload').style.borderRadius = '100px';
         var minunts = 0
-        r=1
+        r = 1
     }
     if (L % 2 != 0) {
         document.getElementById("audio").loop = false;
         document.getElementById('reload').style.backgroundColor = 'transparent'
-        r=0;
+        r = 0;
     }
 }
 // CRIAR PLAYLISTS
@@ -120,21 +120,21 @@ function playcreats() {
 // BARRA DE PROGRESSO
 var audio = document.getElementById("audio");
 
-audio.addEventListener('timeupdate', function() {
+audio.addEventListener('timeupdate', function () {
     var duration = audio.duration;
     var current = audio.currentTime;
-    document.getElementById("progress_bar").style.width = ((current / duration) * 100)  + "%"
+    document.getElementById("progress_bar").style.width = ((current / duration) * 100) + "%"
 });
 
 // TEMPO QUE FALTA PARA A MUSICA CORRER
 function conversao() {
-    setInterval(function() {
+    setInterval(function () {
 
         teste = (document.getElementById('audio').duration - document.getElementById('audio').currentTime);
         var minutos = (Math.trunc(teste / 60));
         var segundos = Math.trunc(teste % 60);
-        if(segundos < 10){
-            segundos = "0"+segundos
+        if (segundos < 10) {
+            segundos = "0" + segundos
         }
         var xixi = (minutos + ":" + segundos);
 
@@ -144,46 +144,68 @@ function conversao() {
 }
 
 // TEMPO QUE A MUSICA PERCORREU
- function current(){
-     var time;
-    if (document.getElementById('audio').currentTime == 0){
+function current() {
+    var time;
+    if (document.getElementById('audio').currentTime == 0) {
         minunts = 0
     }
-     setInterval(function(){
-    current = document.getElementById('audio').currentTime
-    time = current % 60
-    if(time < 9){
-        document.getElementById("time1").innerHTML = minunts+":0"+time.toFixed(0);
-    }else{
-    document.getElementById("time1").innerHTML = minunts+":"+time.toFixed(0);
-}
-    if (time >= 59){
-        minunts += 1;
-    }
-    if(r==0){
-        if(document.getElementById('audio').duration<=document.getElementById('audio').currentTime){
-            next()
+    setInterval(function () {
+        current = document.getElementById('audio').currentTime
+        time = current % 60
+        if (time < 9) {
+            document.getElementById("time1").innerHTML = minunts + ":0" + time.toFixed(0);
+        } else {
+            document.getElementById("time1").innerHTML = minunts + ":" + time.toFixed(0);
         }
-    }else{
-        minunts = 0
-        time =0;
-    }
-    
- },1000);
+        if (time >= 59) {
+            minunts += 1;
+        }
+        if (r == 0) {
+            if (document.getElementById('audio').duration <= document.getElementById('audio').currentTime) {
+                next()
+            }
+        } else {
+            minunts = 0
+            time = 0;
+        }
 
- }
- function add1(){
-     document.getElementById("msc1").innerHTML = "Neovaii Take It Back <br>"
+    }, 1000);
 
- }
- function add2(){
-    document.getElementById("msc2").innerHTML = "Stephen - Crossfire"
-}
-function add3(){
-    document.getElementById("msc3").innerHTML = "AWAY & Midoca"
-}
-function add4(){
-    document.getElementById("msc4").innerHTML = "Lux Holm & Glaceo"
 }
 
- 
+var msc = [{ name: "", src: "", imagem: "" },
+    { name: "", src: "", imagem: "" },
+    { name: "", src: "", imagem: "" },
+    { name: "", src: "", imagem: "" }
+]
+function add1() {
+    document.getElementById("msc1").innerHTML = "--Neovaii Take It Back <br>"
+    msc[0].src.push('Biblioteca/musica1.mp3')
+    msc[0].name.push('Neovaii Take It Back')
+    msc[0].imagem.push('fundo/imagem1.jpg')
+    console.log(msc)
+}
+function add2() {
+    document.getElementById("msc2").innerHTML = "--Stephen - Crossfire"
+    msc[1].src.push('Biblioteca/musica2.mp3')
+    msc[1].name.push('Stephen - Crossfire')
+    msc[1].imagem.push('fundo/imagem2.jpg')
+    console.log(msc)
+}
+function add3() {
+    document.getElementById("msc3").innerHTML = "--AWAY & Midoca"
+    msc[2].src.push('Biblioteca/musica3.mp3')
+    msc[2].name.push('AWAY & Midoca')
+    msc[2].imagem.push('fundo/imagem3.jpg')
+    console.log(msc)
+}
+function add4() {
+    document.getElementById("msc4").innerHTML = "--Lux Holm & Glaceo"
+    msc[3].src.push('Biblioteca/musica4.mp3')
+    msc[3].name.push('Lux Holm & Glaceo')
+    msc[3].imagem.push('fundo/imagem4.jpg')
+    console.log(msc)
+}
+
+
+
